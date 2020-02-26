ed1e2d647bc46144b1caa0b5c7f383761e4b30f4
Md Jahangir Alam <mza0150@auburn.edu>
2020-02-26 11:29:51 -0600

    logged in info formatting correclty

--- part_2/sys_info_ALAM.sh
+++ part_2/sys_info_ALAM.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
 # This script will invoke current time  and date, a list of all logged-in users id and the system uptime.
+echo >> time_date_logged_id_uptime.log
+echo >> time_date_logged_id_uptime.log
 date_var=`date|cut -d " " -f1-3,4|awk '{print "Current Time is: " $4 "--------------------" " and Todays Date is: " $2,$1,$3}'`
 echo ${date_var} >> time_date_logged_id_uptime.log
-
+echo >> time_date_logged_id_uptime.log
+echo >> time_date_logged_id_uptime.log
 echo "*******************************" >> time_date_logged_id_uptime.log
 # Find logged-in user-ID:
 logged_user=`who | cut -d' ' -f1 | sort | uniq`
 
-echo "Currently logged users name: $logged_user" >> time_date_logged_id_uptime.log
-
+echo "Currently logged users name:" >> time_date_logged_id_uptime.log
+echo  "$logged_user " >> time_date_logged_id_uptime.log
+echo >> time_date_logged_id_uptime.log
+echo >> time_date_logged_id_uptime.log
 #Get system uptime info:
 system_uptime=`uptime`
 
 echo "******************************" >> time_date_logged_id_uptime.log
+echo >> time_date_logged_id_uptime.log
+echo >> time_date_logged_id_uptime.log
 echo "System Uptime: $system_uptime" >>  time_date_logged_id_uptime.log
-
+echo >>  time_date_logged_id_uptime.log
+echo >>  time_date_logged_id_uptime.log
+echo "******************************" >>  time_date_logged_id_uptime.log
