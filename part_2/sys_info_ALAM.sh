eaf4a9638381db96d235c74390d3b43bbe101554
Md Jahangir Alam <mza0150@auburn.edu>
2020-02-26 10:50:48 -0600

    functioning script is ready

--- part_2/sys_info_ALAM.sh
+++ part_2/sys_info_ALAM.sh
@@ -2,7 +2,7 @@
 
 # This script will invoke current time  and date, a list of all logged-in users id and the system uptime.
 date_var=`date|cut -d " " -f1-3,4|awk '{print "Current Time is: " $4 "--------------------" " and Todays Date is: " $2,$1,$3}'`
-echo ${date_var} > time_date_logged_id_uptime.log
+echo ${date_var} >> time_date_logged_id_uptime.log
 
 echo "*******************************" >> time_date_logged_id_uptime.log
 # Find logged-in user-ID:
